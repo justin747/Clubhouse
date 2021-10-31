3c83fbdbd66dbd5532a58a3bb44d8b1b8aa6dc2a
Justin747 <justin747@gmail.com>
2021-10-30 17:57:50 -0700

    Created HomeHeaderView
    Created HomeHeaderView

---
+++ Views/Headers/HomeHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeHeaderView.swift
+//  Clubhouse
+//
+//  Created by Justin747 on 10/30/21.
+//
+
+import SwiftUI
+
+struct HomeHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeHeaderView()
+    }
+}
