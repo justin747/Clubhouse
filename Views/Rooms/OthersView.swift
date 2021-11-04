e792c3ae767a52628b8115224abf907146bcc210
Justin747 <justin747@gmail.com>
2021-11-03 19:02:53 -0700

    Created OthersView
    Created OthersView which will hold a LazyVStack to display list of users in a room

---
+++ Views/Rooms/OthersView.swift
@@ -0,0 +1,20 @@
+//
+//  OthersView.swift
+//  Clubhouse
+//
+//  Created by Justin747 on 11/3/21.
+//
+
+import SwiftUI
+
+struct OthersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OthersView_Previews: PreviewProvider {
+    static var previews: some View {
+        OthersView()
+    }
+}
