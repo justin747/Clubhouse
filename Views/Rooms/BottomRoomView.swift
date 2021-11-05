a6e4569c43049a7335910a086e6a9b92d4867b2b
Justin747 <justin747@gmail.com>
2021-11-04 22:07:27 -0700

    Created HostsView
    Created HostsView to display the avatar of Hosts larger than the avatars of the general users in a room

---
+++ Views/Rooms/BottomRoomView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomRoomView.swift
+//  Clubhouse
+//
+//  Created by Justin747 on 11/4/21.
+//
+
+import SwiftUI
+
+struct BottomRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomRoomView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomRoomView()
+    }
+}
